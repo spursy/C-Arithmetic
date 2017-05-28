3cd30087d1dfba3bf4230dd33dd6820cd1cb77d9
spursy <582214816@qq.com>
2017-05-28 15:30:17 +0800

    add c## synxtax demo.

---
+++ SyntaxDemo/demo.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+ int main(void) { 
+     cout<< "Please input a number:" << endl;
+     int x = 0;
+     cin>>x;
+     cout<<oct<<x<<endl;
+     cout<<dec<<x<<endl;
+     cout<<hex<<x<<endl;
+     return 0;  
+}
